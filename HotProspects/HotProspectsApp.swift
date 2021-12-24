502a20f276fd1cde77b9df0083131673f85f5908
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-24 10:10:24 +0800

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Giovanni Gaffé on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
