5c6f3fa7ce38f0b585ae49c4d2864dab2c52996a
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-30 14:16:25 +0800

    set navigation view and generate code

--- HotProspects/Views/MeView.swift
+++ HotProspects/Views/MeView.swift
@@ -5,11 +5,54 @@
 //  Created by Giovanni Gaffé on 2021/12/30.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var emailAdresss = "you@yoursite.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+                
+                TextField("Email adress", text: $emailAdresss)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+                
+                
+                Image(uiImage: generateQRCode(from: qrCode))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+
+                
+                Spacer()
+                
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
