41775e740ac4594c35394edaa160e197fc732091
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-30 10:31:47 +0800

    set title and filter

--- HotProspects/Views/ProspectsView.swift
+++ HotProspects/Views/ProspectsView.swift
@@ -8,13 +8,34 @@
 import SwiftUI
 
 struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    let filter: FilterType
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text("Hello world !")
+                .navigationTitle(title)
+        }
+        
     }
 }
 
 struct ProspectsView_Previews: PreviewProvider {
     static var previews: some View {
-        ProspectsView()
+        ProspectsView(filter: .none)
     }
 }
