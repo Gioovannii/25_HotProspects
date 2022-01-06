c57e310a76a226de402c6cbb0f625a9b8575492f
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-01-06 14:31:48 +0800

    add context menu on qr code

--- HotProspects/Views/MeView.swift
+++ HotProspects/Views/MeView.swift
@@ -5,42 +5,52 @@
 //  Created by Giovanni Gaffé on 2021/12/30.
 //
 
+import CoreImage
 import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAdresss = "you@yoursite.com"
-
+    @State private var qrCode = UIImage()
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
                 TextField("Name", text: $name)
                     .textContentType(.name)
                     .font(.title)
-                    .padding(.horizontal)
                 
                 TextField("Email adress", text: $emailAdresss)
                     .textContentType(.emailAddress)
                     .font(.title)
-                    .padding([.horizontal, .bottom])
                 
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAdresss)"))
-                    .interpolation(.none)
+                Image(uiImage: qrCode)
                     .resizable()
+                    .interpolation(.none)
                     .scaledToFit()
                     .frame(width: 200, height: 200)
-
-                
-                Spacer()
-                
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
+                    .navigationTitle("Your code")
+                    .onAppear(perform: updateCode)
+                    .onChange(of: name) { _ in updateCode() }
+                    .onChange(of: emailAdresss) { _ in updateCode() }
             }
-            .navigationTitle("Your code")
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAdresss)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
@@ -50,6 +60,7 @@ struct MeView: View {
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
                 return UIImage(cgImage: cgimg)
+                
             }
         }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
